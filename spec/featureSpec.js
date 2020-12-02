ce34d0e4cae01b7e7656a18b7cd78096486a67ed
francescoFH <francescofacca@gmail.com>
2020-12-02 18:26:53 +0000

    Completed user story 1

---
+++ spec/featureSpec.js
@@ -0,0 +1,16 @@
+'use strict'; //You can use strict mode in all your programs. It helps you to write cleaner code, like preventing you from using undeclared variables.
+
+describe('Feature Test:', function(){
+  var plane;
+  var airport;
+
+  beforeEach(function(){
+    plane = new Plane();
+    airport = new Airport();
+  });
+
+  it('planes can be instructed to land at an airport', function(){
+    plane.land(airport);
+    expect(airport.planes()).toContain(plane);
+  });
+});
