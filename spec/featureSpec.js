c7b5c72d8edd45ed50007f8fba876795ee7e128f
francescoFH <francescofacca@gmail.com>
2021-02-17 12:37:43 +0000

    Build: Green - Added guard clause on takeoff method to don't allow to takeoff if stormy

--- spec/featureSpec.js
+++ spec/featureSpec.js
@@ -20,10 +20,10 @@ describe('Feature Test:', function(){
     expect(airport.planes()).not.toContain(plane)
   });
 
-  it('blocks takeoff when weather is stormy', function() {
+  it('blocks takeoff when weather is stormy', function(){
     plane.land(airport)
-    spyOn(airport, 'isStormy').and.returnValue(true);
-    expect(function(){ plane.takeoff();}).toThrowError('cannot take off during storm');
+    spyOn(airport,'isStormy').and.returnValue(true);
+    expect(function(){ plane.takeoff();}).toThrowError('cannot takeoff during storm');
     expect(airport.planes()).toContain(plane);
   });
 });
--- src/airport.js
+++ src/airport.js
@@ -11,6 +11,9 @@ class Airport{
     this._hangar.push(plane);
   }
   clearForTakeOff(plane) {
+    if(this.isStormy()) {
+      throw new Error('cannot takeoff during storm')
+    }
     this._hangar = [];
   }
   isStormy() {
